63124d2dcea01a2515f03ae9f7dd20719575d99c
Marie Ilic <ilic.m19@htlwienwest.at>
2022-02-03 14:18:53 +0100

    Projektdateien hinzufügen.

---
+++ firstgitupproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstgitupproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
